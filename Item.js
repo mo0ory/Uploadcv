fec47a944f67f476c63bf7e047b15c970e29b0e7
mo0ory <su77mmar@gmail.com>
2019-07-25 07:57:09 +0300

    Create Item.js

---
+++ Item.js
@@ -0,0 +1,40 @@
+
+const mongoose=require('mongoose');
+const Schema= mongoose.Schema
+//Create Schema
+const ItemSchema = new Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    university:{
+        type:String,
+        required:true
+    },
+    major:{
+        type:String,
+        required:true
+    },
+    cv:{
+        type:Object,
+        required:true }
+
+});
+
+// eslint-disable-next-line no-undef
+module.exports=Item= mongoose.model('item',ItemSchema);
+
+
+
+
+
+
+
+
+
+
+
