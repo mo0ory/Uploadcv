a5c6b8355eaa74aaa0b0a4ae4410c9dc7a9374fb
mo0ory <su77mmar@gmail.com>
2019-07-27 09:13:28 +0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,24 +1,25 @@
-
 var express = require('express');
 var mongoose = require('mongoose');
-const items = require('./routes/Api/items');
+const items = require('./routes/api/items');
 var bodyParser = require('body-parser');
 var multer = require('multer')
 var cors = require('cors');
 
 //set up express app
 const app = express();
-
+app.use(express.json());
 app.use(cors);
 
 // // bodyparser middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/Api/items', items);
+app.use('/api/items', items);
+
 //connect database
 var url = 'mongodb://localhost:27017/information';
 
-mongoose.connect(url, { useNewUrlParser: true })
+mongoose.connect(url, { useNewUrlParser: true,  useCreateIndex: true })
   .then(() => console.log('MongoDB Connected ...'))
   .catch(err => console.log(err))
 
@@ -32,28 +33,28 @@ var storage = multer.diskStorage({
   }
 })
 
-var upload = multer({ storage: storage }).single('file')
+var upload = multer({ storage: storage }).single('file');
 
-// app.get('/', function (req, res) {
-//   return res.send('Hello Server')
-// })
-app.post('/upload', function (req, res) {
+app.get('/', function (req, res) {
+  return res.send('Hello Server')
+})
 
+app.post( function (req, res) {
   upload(req, res, function (err) {
     if (err instanceof multer.MulterError) {
       return res.status(500).json(err)
     } else if (err) {
       return res.status(500).json(err)
     }
-    // res.send([req.files[0].filename]);
+    res.send([req.files[0].filename]);
     return res.status(200).send(req.file)
 
   })
 
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () =>
-
   console.log('server started on port 3000')
 );
+
